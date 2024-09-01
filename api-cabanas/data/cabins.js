63f8a52c75b56f7266234bb1ac16cf1900bc4398
Manuel Vera <manuel.vera.poblete@gmail.com>
2024-09-01 13:07:30 -0400

    codigo base con api

--- api-cabanas/data/cabins.js
+++ api-cabanas/data/cabins.js
@@ -1,32 +1,13 @@
 // api/data/cabins.js
 
 const cabins = [
-    { id: 1, name: 'Cabaña en Santiago', city: 'Santiago', image: '1.jpeg', description: 'Hermosa cabaña en Santiago, cerca de la naturaleza.' },
-    { id: 2, name: 'Cabaña en Valparaíso', city: 'Valparaíso', image: '2.jpeg', description: 'Cabaña con vista al mar en Valparaíso.' },
-    { id: 3, name: 'Cabaña en La Serena', city: 'La Serena', image: '3.jpeg', description: 'Cabaña acogedora en La Serena, ideal para vacaciones.' },
-    { id: 4, name: 'Cabaña en Pucón', city: 'Pucón', image: '1.jpeg', description: 'Cabaña rústica en Pucón, perfecta para el descanso.' },
-    { id: 5, name: 'Cabaña 2 en Santiago', city: 'Santiago', image: '2.jpeg', description: 'Otra cabaña en Santiago, ideal para la familia.' },
-    { id: 6, name: 'Cabaña 2 en Valparaíso', city: 'Valparaíso', image: '3.jpeg', description: 'Cabaña moderna en Valparaíso, con todas las comodidades.' },
-    { id: 7, name: 'Cabaña en Santiago', city: 'Santiago', image: '1.jpeg', description: 'Hermosa cabaña en Santiago, cerca de la naturaleza.' },
-    { id: 8, name: 'Cabaña en Valparaíso', city: 'Valparaíso', image: '2.jpeg', description: 'Cabaña con vista al mar en Valparaíso.' },
-    { id: 9, name: 'Cabaña en La Serena', city: 'La Serena', image: '3.jpeg', description: 'Cabaña acogedora en La Serena, ideal para vacaciones.' },
-    { id: 10, name: 'Cabaña en Pucón', city: 'Pucón', image: '1.jpeg', description: 'Cabaña rústica en Pucón, perfecta para el descanso.' },
-    { id: 11, name: 'Cabaña 2 en Santiago', city: 'Santiago', image: '2.jpeg', description: 'Otra cabaña en Santiago, ideal para la familia.' },
-    { id: 12, name: 'Cabaña 2 en Valparaíso', city: 'Valparaíso', image: '3.jpeg', description: 'Cabaña moderna en Valparaíso, con todas las comodidades.' },
-    { id: 13, name: 'Cabaña en Santiago', city: 'Santiago', image: '1.jpeg', description: 'Hermosa cabaña en Santiago, cerca de la naturaleza.' },
-    { id: 14, name: 'Cabaña en Valparaíso', city: 'Valparaíso', image: '2.jpeg', description: 'Cabaña con vista al mar en Valparaíso.' },
-    { id: 15, name: 'Cabaña en La Serena', city: 'La Serena', image: '3.jpeg', description: 'Cabaña acogedora en La Serena, ideal para vacaciones.' },
-    { id: 16, name: 'Cabaña en Pucón', city: 'Pucón', image: '1.jpeg', description: 'Cabaña rústica en Pucón, perfecta para el descanso.' },
-    { id: 17, name: 'Cabaña 2 en Santiago', city: 'Santiago', image: '2.jpeg', description: 'Otra cabaña en Santiago, ideal para la familia.' },
-    { id: 18, name: 'Cabaña 2 en Valparaíso', city: 'Valparaíso', image: '3.jpeg', description: 'Cabaña moderna en Valparaíso, con todas las comodidades.' },
-    { id: 19, name: 'Cabaña en Santiago', city: 'Santiago', image: '1.jpeg', description: 'Hermosa cabaña en Santiago, cerca de la naturaleza.' },
-    { id: 20, name: 'Cabaña en Valparaíso', city: 'Valparaíso', image: '2.jpeg', description: 'Cabaña con vista al mar en Valparaíso.' },
-    { id: 21, name: 'Cabaña en La Serena', city: 'La Serena', image: '3.jpeg', description: 'Cabaña acogedora en La Serena, ideal para vacaciones.' },
-    { id: 22, name: 'Cabaña en Pucón', city: 'Pucón', image: '1.jpeg', description: 'Cabaña rústica en Pucón, perfecta para el descanso.' },
-    { id: 23, name: 'Cabaña 2 en Santiago', city: 'Santiago', image: '2.jpeg', description: 'Otra cabaña en Santiago, ideal para la familia.' },
-    { id: 24, name: 'Cabaña 2 en Valparaíso', city: 'Valparaíso', image: '3.jpeg', description: 'Cabaña moderna en Valparaíso, con todas las comodidades.' },
-  
+    { id: 1, name: 'Cabaña en Quillon', city: 'Quillon', image: '1.jpeg', description: 'Hermosa cabaña en Quillon, cerca de la naturaleza.' },
+    { id: 5, name: 'Cabaña 2 en Quillon', city: 'Quillon', image: '2.jpeg', description: 'Otra cabaña en Quillon, ideal para la familia.' },
+    { id: 7, name: 'Cabaña en Quillon', city: 'Quillon', image: '3.jpeg', description: 'Hermosa cabaña en Quillon, cerca de la naturaleza.' },
+    { id: 11, name: 'Cabaña 2 en Quillon', city: 'Quillon', image: '1.jpeg', description: 'Otra cabaña en Quillon, ideal para la familia.' },
+    { id: 17, name: 'Cabaña 2 en Quillon', city: 'Quillon', image: '2.jpeg', description: 'Otra cabaña en Quillon, ideal para la familia.' },
+    { id: 19, name: 'Cabaña en Quillon', city: 'Quillon', image: '3.jpeg', description: 'Hermosa cabaña en Quillon, cerca de la naturaleza.' },
+    { id: 23, name: 'Cabaña 2 en Quillon', city: 'Quillon', image: '1.jpeg', description: 'Otra cabaña en Quillon, ideal para la familia.' },
   ];
-  
   module.exports = cabins;
   
